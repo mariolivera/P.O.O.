ff3c6ad5c31ea006aa5574f38232fd8825213489
mariolivera <mariooliveiratec@hotmail.com>
2022-11-27 11:13:11 -0300

    lista de alunos controller

--- POO/crudphpoo/config/database.php
+++ POO/crudphpoo/config/database.php
@@ -1,5 +1,10 @@
 <?php
 
+// const DB_USER = 'root';
+// const DB_PASSWORD = '1234';
+// const DB_NAME = 'db_escola';
+
+
 const DB_USER = 'root';
-const DB_PASSWORD = '1234';
+const DB_PASSWORD = 'APOcalipisi@66';
 const DB_NAME = 'db_escola';
--- POO/crudphpoo/src/Controller/AlunoController.php
+++ POO/crudphpoo/src/Controller/AlunoController.php
@@ -2,9 +2,12 @@
 
 declare(strict_types=1);
 
-namespace App\Controller;
 
+use App\Model\Aluno;
 use App\Repository\AlunoRepository;
+use Dompdf\Dompdf;
+use Exception;
+
 
 class AlunoController extends AbstractController
 {
@@ -22,17 +25,81 @@ class AlunoController extends AbstractController
 
     public function cadastrar():void
     {
+        if (true === empty($_POST)){
         $this->render('aluno/cadastrar');
+        return;
     }
 
-    public function excluir(): void
-    {
-        $this->render('aluno/excluir');
+    $aluno = new Aluno();
+    $aluno ->nome = $_POST['nome'];
+    $aluno ->dataNascimento = $_POST['nascimento'];
+    $aluno ->cpf = $_POST['cpf'];
+    $aluno ->email = $_POST['email'];
+    $aluno ->genero = $_POST['genero'];
+
+    $rep = new AlunoRepository();
+        try {
+            $rep->inserir($aluno);
+        } catch (Exception $exception){
+            if (true === strict_contains($exception->getMessage(), 'cpf')){
+                die('CPF ja existe');
+            }
+            if (true === strict_contains($exception->getMessage(), 'email')){
+                die('Email ja existe');
+            }
+            die('Aconteceu um erro, estamos trabalando para solucionar o problema mais rapido');
+        }
+        $this->redirect('/aluno/listar');
     }
 
     public function editar(): void
     {
-        $this->render('aluno/editar');
+        $id = $_GET['id'];
+        $rep = new AlunoRepository();
+        $aluno =$rep->buscarUm($id);
+        $this->render('aluno/editar', [$aluno]);
+            if (false === empty($$_POST)) {
+                $aluno ->nome = $_POST['nome'];
+                $aluno ->dataNascimento = $_POST['nascimento'];
+                $aluno ->cpf = $_POST['cpf'];
+                $aluno ->email = $_POST['email'];
+                $aluno ->genero = $_POST['genero'];
+                
+                try {
+                    $rep->atualizar($aluno, $id);
+                } catch (Exception $exception){
+                    if (true === strict_contains($exception->getMessage(), 'cpf')){
+                        die('CPF ja existe');
+                    }
+                    if (true === strict_contains($exception->getMessage(), 'email')){
+                        die('Email ja existe');
+                    }
+                    die('Eita, deu BO Ó, vou resolver aqui jájá o site ta no ar');
+                }
+                $this->redirect('alunos/listar');
+            }
     }
     
+    public function excluir(): void
+    {
+        //$this->render('aluno/excluir');
+        $id = $_GET['id'];
+        $rep = new AlunoRepository();
+        $rep-excluir($id);
+        $this->redirect('alunos/listar');
+    }
+    public function relatorio():void
+    {
+        $hoje = date('d/m/Y');
+        $design = "
+            <h1>Relatorio de Alunos</h1>
+            <hr>
+            <em>Gerado em {$hoje}</em>
+        ";
+        $dompdf = new Dompdf();
+        $dompdf->setPaper('A4', 'portrait');
+        $dompdf->loadHtml($design);
+        $dompdf->render();
+        $dompdf->stream();
+    }
 }
